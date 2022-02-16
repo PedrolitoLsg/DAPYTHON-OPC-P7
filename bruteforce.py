52071993e3f847ec4d308b875dc08f1f0cb80c47
Pierre Lesage <84906663+PedrolitoLsg@users.noreply.github.com>
2022-02-16 12:34:56 +0100

    linting

--- bruteforce.py
+++ bruteforce.py
@@ -4,6 +4,7 @@ import csv
 
 piggy_bank = 500
 
+
 def brute(file):
     start = datetime.now()
     print("File used for computing is :" + str(file))
@@ -17,7 +18,7 @@ def brute(file):
 def open_file(file):
     shares = open(file)
     csvreader = csv.reader(shares)
-    header = next(csvreader)
+    next(csvreader)
     rows = []
     for row in csvreader:
         rows.append(row)
@@ -25,26 +26,25 @@ def open_file(file):
     pass
 
 
-
 def bruteforce(rows):
     competition_list = [0]
     competition_benefit = [0]
     competition_spendings = [0]
     for elem in range(0, len(rows)+1):
-       for subset in itertools.combinations(rows, elem):
-           spendings = 0
-           benefit = 0
-           for stock in subset:
-               spendings += float(stock[1])
-               benefit += (float(stock[1]) * (float(stock[2])/100))
-           if benefit > competition_benefit[0]:
-               if spendings <= piggy_bank:
-                   del competition_list[0]
-                   competition_list.append(subset)
-                   del competition_benefit[0]
-                   competition_benefit.append(benefit)
-                   del competition_spendings[0]
-                   competition_spendings.append(spendings)
+        for subset in itertools.combinations(rows, elem):
+            spendings = 0
+            benefit = 0
+            for stock in subset:
+                spendings += float(stock[1])
+                benefit += (float(stock[1]) * (float(stock[2])/100))
+            if benefit > competition_benefit[0]:
+                if spendings <= piggy_bank:
+                    del competition_list[0]
+                    competition_list.append(subset)
+                    del competition_benefit[0]
+                    competition_benefit.append(benefit)
+                    del competition_spendings[0]
+                    competition_spendings.append(spendings)
     print("Best Combination gives a benefit of: " + str(competition_benefit) + " euros \n"
           + "The list of shares is" + str(competition_list) + "\n"
           + "total cost = " + str(competition_spendings) + " euros")
